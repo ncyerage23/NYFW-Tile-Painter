45aa7d70f41439e72e2cd744ec240d0bcf5c89ce
Nathaniel Y <ncyerage@gmail.com>
2026-06-20 18:30:29 -0400

    fixed some dumb mistakes and came up with some new ideas

--- main.c
+++ main.c
@@ -138,26 +138,54 @@ void shutdown()
 
 // mouse
 int mx = 10, my = 10;
-int mspeedx = 5, mspeedy = 4;
+float m_sensitivity_y = 1.5;
+float m_sensitivity_x = 2.0;
 
-void draw_mouse()
+void undraw_mouse()
 {
-	// nyfw_canvSetPixel(scr, mx, my, 0xffff); 
-	
-	CANV_PIXEL(scr, mx, my) = 0xffff;
-	CANV_PIXEL(scr, mx-1, my) = 0xffff;
-	CANV_PIXEL(scr, mx+1, my) = 0xffff;
-	CANV_PIXEL(scr, mx, my-1) = 0xffff;
-	CANV_PIXEL(scr, mx, my+1) = 0xffff;
+	nyfw_canvSetPixel(scr, mx, my, 0);
+	nyfw_canvSetPixel(scr, mx-1, my, 0);
+	nyfw_canvSetPixel(scr, mx+1, my, 0);
+	nyfw_canvSetPixel(scr, mx, my-1, 0);
+	nyfw_canvSetPixel(scr, mx, my+1, 0);
 
 }
 
 
-int main() 
+void draw_mouse()
 {
+	nyfw_canvSetPixel(scr, mx, my, 0xffff);
+	nyfw_canvSetPixel(scr, mx-1, my, 0xffff);
+	nyfw_canvSetPixel(scr, mx+1, my, 0xffff);
+	nyfw_canvSetPixel(scr, mx, my-1, 0xffff);
+	nyfw_canvSetPixel(scr, mx, my+1, 0xffff);
+
+}
+
+
+int main(int argc, char* argv[]) 
+{
+	// TODO: okay. This is basically good, minus the lack of colors. That'll come in a minute. 
+	// Here's the plan. I'm gonna use command-line arguments for the file being edited. If the file exists, 
+	// then we open the .nymg (unless it's not 8x8, I guess) and allow editing. If it doesn't, we start blank, 
+	// and on close, save it to that name. 
+	//
+	// TODO: also, gonna need to clean this bitch up quite a bit. Before I do the ^ thing. Put the tile module stuff
+	// in a different file, and make a header for everything (including the future palette module). this file should (obv)
+	// just be for main stuff. That's it. init-update-draw, and the mainloop. 
+	//
+	// Idk. Should be a bit of work, but nothing too bad, right? Lol. When I start trying to do text input arguments, that's gonna
+	// be painful. Hmm. Maybe doing getchar input would be better? Like, not using the mouse (except for special occasions...like a mode)
+	// I mean, it'd be easier. Ooh! Maybe, when pressing a specific key (like ":") it exits mouse input mode, and does getchar. That could work. yeah!
+	// Figure that out in a bit. After I get the rest of this bs done. 
+
+
+	if (argc > 1) printf("%s\n", argv[1]);
+
 	if (!setup()) return 1;
 	
 	int sw = scr.width, sh = scr.height;
+	nyfw_canvasClear(scr);
 
 	while (1) {
 		nyfw_inputPoll();
@@ -165,17 +193,17 @@ int main()
 		// update (TODO: make this a separate function)
 		if (nyfw_inputKeyPressed(NYFW_KEY_ESC)) break;
 
-		mx += nyfw_inputMouseDX() * mspeedx;
-		my += nyfw_inputMouseDY() * mspeedy;
-		
-		mx = MAX(5, MIN(mx, sw-5));
-		my = MAX(5, MIN(my, sh-5));
+		int mdx = (float)nyfw_inputMouseDX() * m_sensitivity_x;
+		int mdy = (float)nyfw_inputMouseDY() * m_sensitivity_y;
 
 		if (nyfw_inputMBPressed(NYFW_MB_L)) tile_check_input(mx, my);
 
-		// draw
-		nyfw_canvasClear(scr);
+		// draw (TODO: make this also a separate function)
+		// nyfw_canvasClear(scr);
+		undraw_mouse();
 		tile_mod_draw();
+		mx += mdx;
+		my += mdy;
 		draw_mouse();
 
 		// present
